0ee1bf8df207c3a9f16d1621058f49773162ab45
mhernan80 <56091989+mhernan80@users.noreply.github.com>
2019-10-30 14:57:23 -0400

    Switch pin # from 2 to 6 in LCD Button Counter

--- Intermediate_Arduino/LCD_ButtonCounter/LCD_ButtonCounter.ino
+++ Intermediate_Arduino/LCD_ButtonCounter/LCD_ButtonCounter.ino
@@ -14,13 +14,13 @@ void setup()
 
 void loop()
 {
-	if(on_off == 0&&digitalRead(2)==1){//if the button is pressed and it previously wasn't pressed
+	if(on_off == 0&&digitalRead(6)==1){//if the button is pressed and it previously wasn't pressed
 		on_off = 1;//set it so it knows that the button was just on
 		counter++;
 		lcd.setCursor(0,1);
 		lcd.print(counter);
 	}
-	if(on_off == 1&&digitalRead(2)==0){//if the button isn't on and it was just on
+	if(on_off == 1&&digitalRead(6)==0){//if the button isn't on and it was just on
 		on_off = 0;//set it so it knows it is off and it is ready to be re-added to
 	}
 }
